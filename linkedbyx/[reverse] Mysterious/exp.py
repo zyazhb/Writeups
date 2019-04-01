bbfd88695c704b7a4a0606224c015f14365f6494
ZYA <zyazhb@yahoo.com>
2019-04-01 11:28:04 +0800

    Create exp.py

---
+++ linkedbyx/[reverse] Mysterious/exp.py
@@ -0,0 +1,9 @@
+if ( strlen(&String) > 6 )
+        ExitProcess(0);
+      v10 = atoi(&String) + 1;
+      if ( v10 == 123 && v12 == 'x' && v14 == 'z' && v13 == 'y' )
+      {
+ 
+ we can know that the key is 6 words
+ and string is 122
+ so the key is 122xyz
