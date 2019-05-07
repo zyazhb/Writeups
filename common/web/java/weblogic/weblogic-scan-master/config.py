770145b3593ea0ee9606ef6090cf4ccf075d3b42
ZYA <zyazhb@yahoo.com>
2019-05-07 11:10:39 +0800

    Add files via upload

---
+++ common/web/java/weblogic/weblogic-scan-master/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: kingkk
+# @Date:   2018-11-08 15:24:08
+# @Last Modified by:   kingkk
+# @Last Modified time: 2018-11-08 19:07:28
+
+TIMEOUT = 5
+
+server = 'http://053r5r.ceye.io' # http://xxx.ceye.io
